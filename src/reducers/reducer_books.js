28bb43a59efb88b156a774b9a0081d753d687246
YOONJAE JUNG/STAFF/LGEUS Americas Information Center(yoonjae.jung@lge.com) <yoonjae.jung@lge.com>
2017-06-26 14:30:23 -0400

    First commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Harry Potter', pages: 101},
+    {title: 'Dark Tower', pages: 39},
+    {title: 'Javascript: The good Parts', pages: 85},
+    {title: 'Eloquent Ruby', pages: 1}
+  ]
+}
